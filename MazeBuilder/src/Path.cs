b56c48346df8af5be319a9a098d9935e9a90832e
michael Steele <michael.steele@checkout.com>
2021-07-26 02:13:35 +0100

    AddToRepo

---
+++ MazeBuilder/src/Path.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeBuilder
+{
+    public class Path : Cell
+    {
+        public Path(int xCoOrd, int yCoOrd, bool visited = false) : base(xCoOrd, yCoOrd, " ")
+        {
+            Visited = visited;
+            IsPath = true;
+        }
+    }
+}
---
+++ MazeBuilder/src/Wall.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeBuilder
+{
+    public class Wall : Cell
+    {
+        public Wall(int xCoOrd, int yCoOrd, bool visited = false) : base(xCoOrd, yCoOrd, "#") {
+            Visited = visited;
+        }
+    }
+}
